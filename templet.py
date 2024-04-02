da185d87ae375befc62855a0d712cb6f9cce7a4f
Sumanta4073 <sumanta4073@gmail.com>
2024-04-02 21:24:12 +0530

    Create templet.py

---
+++ templet.py
@@ -0,0 +1,42 @@
+import logging
+import os
+from pathlib import Path
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    "src/_init_.py",
+    "src/components/_init_.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evalution.py",
+    "src/pipeline/_init_.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
+    "tests/unit/_init_.py",
+    "tests/integration/_init_.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirement_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb",
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating Directory: {filedir} for file: {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass #create an empty file    
+
